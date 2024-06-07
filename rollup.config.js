cdc50987cc3faff18905f91b10b7bf085cb5f25e
Bailey D'Amour <bailey@damour.xyz>
2024-06-07 16:05:01 -0400

    Update dev dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { babel } from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import { DEFAULT_EXTENSIONS } from '@babel/core';
 
 export default function() {
@@ -22,22 +22,21 @@ export default function() {
             resolve({browser: true}),
             commonjs(),
             typescript({
-                target: 'es2017',
+                target: 'es6',
                 sourceMap: !prod,
                 inlineSourceMap: !prod,
-                inlineSources: !prod,
-                allowSyntheticDefaultImports: true
+                inlineSources: !prod
             }),
             babel({
-                exclude: ['*.html', '**/node_modules/**'],
+                exclude: ['*.html'],
                 extensions: [
                     ...DEFAULT_EXTENSIONS,
                     '.ts',
                     '.tsx'
                 ],
-                babelHelpers: 'runtime'
+                babelHelpers: 'bundled'
             }),
             ...(prod ? [terser()] : [])
         ],
-    }
-};
+    };
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,11 @@
-import * as path from 'path';
-import * as express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express, { Express } from 'express';
 
-const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app: Express = express();
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, '../../public')));
